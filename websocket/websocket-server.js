e300d497fb61e25aa849581bbfea3226d2a679e1
KevinNadeau <kevin.nadeau@hotmail.fr>
2020-05-05 17:16:49 +0200

    webSocket

--- websocket/websocket-server.js
+++ websocket/websocket-server.js
@@ -1,9 +1,13 @@
 const WebSocket = require('ws')
 const webSocketServer = new WebSocket.Server({ port: 3003 });
-  
+
+let messages = [];
+
 webSocketServer.on('connection', webSocket => {
+    webSocket.send(messages.toString())
     webSocket.onmessage = messageEvent => {
         const message = messageEvent.data;
+        messages.push(message)
         webSocketServer.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message);
